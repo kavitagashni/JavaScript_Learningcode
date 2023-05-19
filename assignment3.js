3459a760f8af963e2f82d3f4d4a4315cd78587ed
kavitagashni <107977816+kavitagashni@users.noreply.github.com>
2023-05-19 12:44:56 +0530

    Update assignment3.js

--- assignment3.js
+++ assignment3.js
@@ -11,42 +11,13 @@
 
 
 
-// function findArrayEleCount(array) {
-//      let result = {}
-//     // for (let i = 0; i < array.length; i++) {
-//     //     result[array[i]] = (Number(result[array[i]]) || 0) + 1;
-
-//     // }
-//     // return result
-//     // return array.reduce((acc,el)=>{
-//     //     acc[el] = (acc[el] || 0) + 1;
-//     //     return acc;
-//     // },{})
-
-//     // for (const c of array) {
-//     //     result[c] = (result[c] || 0) + 1;
-//     // }
-//     // return result;
+function findArrayEleCount(array) {
+ let result = {};
+ for (const c of array) {
+        result[c] = (result[c] || 0) + 1;
+    }
+ return result;
  
-// }
-// console.log(findArrayEleCount(['abc', 'def', 'cde', 'abc', 'def', 'elc']));
+}
+console.log(findArrayEleCount(['abc', 'def', 'cde', 'abc', 'def', 'elc']));
 
-//lexical structure (1-2day)
-// data-types (1/2 day)
-// operations data
- // --   String --Number --boolean
- // array and object (2-3)
- // General topic loops, conditional statements, operators  <>!&&||
- 
- 	// Assigning values
-     let val1 = 7;
-     let val2 = -"6";
-     
-     // Logical !(NOT) Operator
-     console.log(!val1);
- 
-     // Logical &&(AND) Operator
-     console.log( val1 && val2 );
-     
-     // Lgical ||(OR) Operator
-     console.log( val1 || val2 );
